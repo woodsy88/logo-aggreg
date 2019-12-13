8fac10a49f192a8e46ca93326cdc0d7034f4d8c4
“Andrew <andrewwoods88@gmail.com>
2019-12-13 17:12:13 -0500

    created a test react component

---
+++ app/javascript/components/Home.js
@@ -0,0 +1,14 @@
+import React from "react"
+import PropTypes from "prop-types"
+class Home extends React.Component {
+  render () {
+    return (
+      <React.Fragment>
+        <h1>Home page</h1>
+        <h2>How are you</h2>
+      </React.Fragment>
+    );
+  }
+}
+
+export default Home
