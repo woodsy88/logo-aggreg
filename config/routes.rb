a6702074a172930f838f2d410293fc8f7476a847
“Andrew <andrewwoods88@gmail.com>
2019-12-13 16:57:26 -0500

    installed devise and react

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,11 @@
 Rails.application.routes.draw do
+ devise_scope :user do
+    get 'login', to: 'devise/sessions#new'
+    get 'signup', to: 'devise/registrations#new'
+  end
+
+  devise_for :users, :controllers => {:registrations => "registrations"}
+  get 'pages/home'
+  root to: 'pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
